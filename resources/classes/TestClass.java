9c5ee90cca9c6b7d4767d7fa71fadb950f299abb
bkushigian <bkushigian@gmail.com>
2018-06-01 19:59:00 -0400

    First commit

---
+++ resources/classes/TestClass.java
@@ -0,0 +1,21 @@
+public class TestClass {
+    public int getThree(){
+        return 1 + 2;
+    }
+
+    public int addOneTwoThree(){
+        return 1 + 2 + 3;
+    }
+
+    public int addOneTwoArg(int a){
+        return 1 + 2 + a;
+    }
+
+    public int addOneArgTwo(int a){
+        return 1 + a + 2;
+    }
+
+    public int addArgOneTwo(int a){
+        return a + 1 + 2;
+    }
+}
