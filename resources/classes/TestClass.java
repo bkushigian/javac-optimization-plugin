f5181b75c8e7c7e8cc302b9f38cc3fddbd455da8
bkushigian <bkushigian@gmail.com>
2018-06-03 15:39:21 -0400

    Added method to TestClass.java

--- resources/classes/TestClass.java
+++ resources/classes/TestClass.java
@@ -1,4 +1,6 @@
 public class TestClass {
+    public int i = 0;
+    public int j = 0;
 
     public int leftConstantFold(int a){
         return 1 + 2 + a;
@@ -6,5 +8,18 @@ public class TestClass {
 
     public int rightConstantFold(int a){
         return a + 1 + 2;
+    }
+
+    public int parenConstantFold(int a) {
+        return a + (1 + 2);
+    }
+
+    public int yearsToSeconds(int years){
+        int seconds = years * 60 * 60 * 24 * 365;
+        return seconds;
+    }
+
+    public int divExample(int a, int b, int c){
+        return a / b / c;
     }
 }
