7fb0b73464df3af20d77067ef500777ad23f9757
bkushigian <bkushigian@gmail.com>
2018-06-03 23:57:56 -0400

    Incoroporated TreeFolder into pluging

--- src/com/tangentiallyrelated/plugin/ConstFoldTreeScanner.java
+++ src/com/tangentiallyrelated/plugin/ConstFoldTreeScanner.java
@@ -1,12 +1,8 @@
 package com.tangentiallyrelated.plugin;
 
-import com.sun.tools.javac.code.TypeTag;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.TreeScanner;
 import com.sun.tools.javac.util.Context;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 public class ConstFoldTreeScanner extends TreeScanner{
 
@@ -24,7 +20,7 @@ public class ConstFoldTreeScanner extends TreeScanner{
 
     @Override
     public void visitBinary(JCTree.JCBinary tree) {
-        System.out.println("   visitBinary: " + tree);
         super.visitBinary(tree);
+        TreeFolder.foldr(tree);
     }
 }
